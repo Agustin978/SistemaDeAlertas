f64981e5f4350ba8d7c717a86994bb1aea37cd49
Agustin Lobo <loboagus10@gmail.com>
2024-09-04 19:14:54 -0300

    Update index.js
    Compruebo que las cosas funcionen

--- src/index.js
+++ src/index.js
@@ -19,20 +19,22 @@ usuario2.suscribeATema(tema2);
 usuario3.suscribeATema(tema2);
 usuario1.suscribeATema(tema2);
 usuario1.suscribeATema(tema1);
+usuario3.suscribeATema(tema3);
 
 
 //Envio de alertas
 const fechaExpira = new Date(Date.now() + 1000 * 60 * 60);
 const fechaExpirada = new Date(Date.now() - 1000 * 60 * 60 * 24); //Fecha retrasada por un dia
-//sistema.enviarAlerta('Informativa', 'Nueva actualizacion disponible.', fechaExpira, tema1.id);
-//sistema.enviarAlerta('Urgente', 'Servidor caido.', fechaExpirada, tema2.id);
-//sistema.enviarAlerta('Urgente', 'Recibio una actualización.', fechaExpira, tema3.id);
-//sistema.enviarAlerta('Informativa', 'Algo extra.', fechaExpira, tema3.id, usuario1.id);
+
 sistema.enviarAlerta('Informativa','I1',fechaExpira,tema1.id,usuario1.id);
 sistema.enviarAlerta('Urgente','U1',fechaExpira, tema2.id); //Noticia que se manda a todos los usuarios registrados
+sistema.enviarAlerta('Informativa','I2',fechaExpirada,tema3.id);
 console.log('Alertas no leidas de usuario',usuario1.getNombre(),'\n',sistema.ObtenerAlertasNoLeidasDeUsuario(usuario1.id));
 console.log('Alertas no leidas de usuario',usuario2.getNombre(),'\n',sistema.ObtenerAlertasNoLeidasDeUsuario(usuario2.id));
 console.log('Alertas no leidas de usuario',usuario3.getNombre(),'\n',sistema.ObtenerAlertasNoLeidasDeUsuario(usuario3.id));
+
+console.log('Alertas no expiradas de tema ',tema2.titulo,'\n',sistema.ObtenerAlertasNoExpiradasDeTema(tema2.id));
+console.log('Alertas no expiradas de tema ',tema3.titulo,'\n',sistema.ObtenerAlertasNoExpiradasDeTema(tema3.id));
 
 
 //Obtener alertas no leidas de un usuario
