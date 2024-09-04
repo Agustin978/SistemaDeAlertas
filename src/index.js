0ea407d80f24694bff634f3860edf37383a8ef9c
Agustin Lobo <loboagus10@gmail.com>
2024-09-04 17:39:36 -0300

    Update index.js
    Modificadas las pruebas

--- src/index.js
+++ src/index.js
@@ -16,17 +16,31 @@ const tema3 = sistema.registrarTema('Feriados en Argentina');
 usuario1.suscribeATema(tema1);
 usuario2.suscribeATema(tema1);
 usuario2.suscribeATema(tema2);
+usuario3.suscribeATema(tema2);
+usuario1.suscribeATema(tema2);
+usuario1.suscribeATema(tema1);
+
 
 //Envio de alertas
-sistema.enviarAlerta('Informativa', 'Nueva actualizacion disponible.', new Date(Date.now() + 1000 * 60 * 60), tema1.id);
-sistema.enviarAlerta('Urgente', 'Servidor caido.', new Date(Date.now() + 1000 * 60 * 60), tema2.id);
+const fechaExpira = new Date(Date.now() + 1000 * 60 * 60);
+const fechaExpirada = new Date(Date.now() - 1000 * 60 * 60 * 24); //Fecha retrasada por un dia
+//sistema.enviarAlerta('Informativa', 'Nueva actualizacion disponible.', fechaExpira, tema1.id);
+//sistema.enviarAlerta('Urgente', 'Servidor caido.', fechaExpirada, tema2.id);
+//sistema.enviarAlerta('Urgente', 'Recibio una actualización.', fechaExpira, tema3.id);
+//sistema.enviarAlerta('Informativa', 'Algo extra.', fechaExpira, tema3.id, usuario1.id);
+sistema.enviarAlerta('Informativa','I1',fechaExpira,tema1.id,usuario1.id);
+sistema.enviarAlerta('Urgente','U1',fechaExpira, tema2.id); //Noticia que se manda a todos los usuarios registrados
+console.log('Alertas no leidas de usuario',usuario1.getNombre(),'\n',sistema.ObtenerAlertasNoLeidasDeUsuario(usuario1.id));
+console.log('Alertas no leidas de usuario',usuario2.getNombre(),'\n',sistema.ObtenerAlertasNoLeidasDeUsuario(usuario2.id));
+console.log('Alertas no leidas de usuario',usuario3.getNombre(),'\n',sistema.ObtenerAlertasNoLeidasDeUsuario(usuario3.id));
+
 
 //Obtener alertas no leidas de un usuario
-console.log('Alertas no leidas de: ',usuario2.getNombre(),'\n', sistema.ObtenerAlertasNoLeidasDeUsuario(usuario2.id));
-console.log('\nAlertas no leidas de: ',usuario1.getNombre(),'\n', sistema.ObtenerAlertasNoLeidasDeUsuario(usuario1.id));
+//console.log('Alertas no leidas de: ',usuario2.getNombre(),'\n', sistema.ObtenerAlertasNoLeidasDeUsuario(usuario2.id));
+//console.log('\nAlertas no leidas de: ',usuario1.getNombre(),'\n', sistema.ObtenerAlertasNoLeidasDeUsuario(usuario1.id));
 //Obtener alertas no expiradas de un tema
-console.log('Alertas no expiradas de ',tema1.titulo,'\n',sistema.ObtenerAlertasNoExpiradasDeTema(tema1.titulo));
+//console.log('Alertas no expiradas de ',tema1.titulo,'\n',sistema.ObtenerAlertasNoExpiradasDeTema(tema1.titulo));
 
 //Envia alerta a un usuario
-sistema.enviarAlerta('Informativa', 'Algo extra.', new Date(Date.now() + 1000 * 60 * 60), tema3.id, usuario1.id);
-console.log('\nSe envia una alerta al usuario: ',usuario1.getNombre(),'\n',sistema.ObtenerAlertasNoLeidasDeUsuario(usuario1.id));
+//console.log('\nSe envia una alerta al usuario: ',usuario1.getNombre(),'\n',sistema.ObtenerAlertasNoLeidasDeUsuario(usuario1.id));
+
