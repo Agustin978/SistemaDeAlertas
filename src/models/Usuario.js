b4d4409748e0fc0f9c15e59d58aaf5fcf00b6b5b
Agustin Lobo <loboagus10@gmail.com>
2024-09-03 21:30:06 -0300

    Create Usuario.js
    Agregado el modelo Usuario en Js

---
+++ src/models/Usuario.js
@@ -0,0 +1,31 @@
+class Usuario
+{
+    static #idCounter = 0;
+
+    constructor(nombre, password)
+    {
+        this.id = Usuario.#idCounter++;
+        this.nombre = nombre;
+        this.password = password;
+        this.temasSuscrito = [];
+        this.alertasNoLeidas = [];
+    }
+
+    suscribeATema(tema){
+        this.temasSuscrito.push(tema);
+    }
+    recibirAlerta(alerta)
+    {
+        this.alertasNoLeidas.push(alerta);
+    }
+    alertaLeida(alerta)
+    {
+        this.alertasNoLeidas = this.alertasNoLeidas.filter(a => a !== alerta);
+    }
+    obtenerAlertasNoLeidas()
+    {
+        return this.alertasNoLeidas;
+    }
+}
+
+module.exports = Usuario;
