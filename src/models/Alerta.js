3504b6fa324c08d264ec617789fb7ff0220ab87c
Agustin Lobo <loboagus10@gmail.com>
2024-09-04 17:36:24 -0300

    Update Alerta.js
    Agregado el id del tema al cual pertenece la alerta y un estado para determinar si la misma ya se leyo.

--- src/models/Alerta.js
+++ src/models/Alerta.js
@@ -1,10 +1,10 @@
 class Alerta
 {
-    static #idCounter = 0;
+    static #idCounter = 1;
     static TIPO_INFORMATIVA = 'Informativa';
     static TIPO_URGENTE = 'Urgente';
 
-    constructor(tipo, mensaje, fechaExpira, paraTodos = false, usuarioParticular = null)
+    constructor(tipo, mensaje, fechaExpira, paraTodos = false, usuarioParticular = null, temaID)
     {
         this.id = Alerta.#idCounter++;
         this.tipo = tipo;
@@ -12,11 +12,17 @@ class Alerta
         this.fechaExpira = fechaExpira;
         this.paraTodos = paraTodos;
         this.usuarioParticular = usuarioParticular;
+        this.temaID = temaID;
+        this.leida = false;
     }
 
     estaExpirada()
     {
         return new Date() > this.fechaExpira;
+    }
+    marcaComoLeida()
+    {
+        this.leida = true;
     }
 }
 
