51ebc5561143c7153fe31d042e26a60607f1fe92
Agustin Lobo <loboagus10@gmail.com>
2024-09-04 17:36:45 -0300

    Update Tema.js
    Agregada funcion para obtener el titulo del tema

--- src/models/Tema.js
+++ src/models/Tema.js
@@ -1,10 +1,15 @@
 class Tema
 {
-    static #idCounter = 0;
+    static #idCounter = 1;
     
     constructor(titulo) {
         this.id = Tema.#idCounter++;
         this.titulo = titulo;
+    }
+
+    obtenerTitulo()
+    {
+        return this.titulo;
     }
 }
 
